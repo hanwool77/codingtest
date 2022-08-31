9c3ef820c45791586f28ce89f6fde18496a920bf
전한울 <wjsgksdnf11@naver.com>
2022-08-31 22:30:52 +0900

    BQJ_2812_도시분할계획_G4

---
+++ 0831/BQJ_1647_도시분할계획_G4.py
@@ -0,0 +1,38 @@
+# 최소 신장 트리 문제
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+edges = []
+result = 0
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+last = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union(parent, a, b)
+        result += cost
+        last = cost
+
+print(result - last)
