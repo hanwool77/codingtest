4e97def62c95222d3b4330b1e003229727c9c8af
전한울 <wjsgksdnf11@naver.com>
2022-09-06 22:22:44 +0900

    BQJ_14497_주난의난_G4

--- 0905/BQJ_14497_주난의난_G4.py
+++ 0905/BQJ_14497_주난의난_G4.py
@@ -11,3 +11,31 @@ graph = [list(input()) for _ in range(n)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
+distance = [[1e9] * m for _ in range(n)]
+distance[x1-1][y1-1] = 0
+
+q = deque()
+q.append((x1-1, y1-1))
+
+while q:
+    x, y = q.popleft()
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == '0':
+                if distance[x][y] < distance[nx][ny]:
+                    distance[nx][ny] = distance[x][y]
+                    q.append((nx, ny))
+            elif graph[nx][ny] == '1':
+                if distance[x][y] + 1 < distance[nx][ny]:
+                    distance[nx][ny] = distance[x][y] + 1
+                    q.append((nx, ny))
+            elif graph[nx][ny] == '#':
+                if distance[x][y] + 1 < distance[nx][ny]:
+                    distance[nx][ny] = distance[x][y] + 1
+                    q.append((nx, ny))
+
+print(distance[x2-1][y2-1])
