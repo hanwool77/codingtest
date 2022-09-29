abb1a3b50a575509b8feebe9b73b67405539a411
전한울 <wjsgksdnf11@naver.com>
2022-09-29 21:33:35 +0900

    BQJ_2110_공유기설치_G4

---
+++ 0929/BQJ_2110_공유기설치_G4.py
@@ -0,0 +1,31 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n, c = map(int, input().split())
+array = []
+for _ in range(n):
+    array.append(int(input()))
+
+array.sort()
+start = 1
+end = array[-1] - array[0]
+result = 0
+
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 1
+    value = array[0]
+
+    for i in range(1, len(array)):
+        if array[i] - value >= mid:
+            count += 1
+            value = array[i]
+
+    if count >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+
+print(result)
