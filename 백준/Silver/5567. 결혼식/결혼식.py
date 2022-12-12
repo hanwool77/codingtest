d328fb11a754d2a5c5bc5013f5a19a6e1406a471
hanwool77 <wjsgksdnf11@naver.com>
2022-12-12 20:45:17 +0900

    [Silver II] Title: 결혼식, Time: 148 ms, Memory: 115208 KB -BaekjoonHub

---
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -0,0 +1,39 @@
+import heapq
+import sys
+input = lambda :sys.stdin.readline().rstrip()
+
+n = int(input())    # 2<= n < = 500
+m = int(input())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+    graph[b].append((a, 1))
+
+INF = int(1e9)
+distance = [INF] * (n + 1)
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for next in graph[now]:
+            cost = dist + next[1]
+            if cost < distance[next[0]]:
+                distance[next[0]] = cost
+                heapq.heappush(q, (cost, next[0]))
+
+dijkstra(1)
+result = 0
+for d in distance:
+    if 1 <= d <= 2:
+        result += 1
+print(result)
+
+
