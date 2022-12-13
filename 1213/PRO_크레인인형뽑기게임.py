1519ca78eba6d6a4d9717762bba7ea7c4e98c66c
hanwool77 <wjsgksdnf11@naver.com>
2022-12-13 11:32:13 +0900

    PRO_크레인 인형뽑기 게임

---
+++ 1213/PRO_크레인인형뽑기게임.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def solution(board, moves):
+    answer = 0
+    stack = deque()
+
+    n = len(board)
+    for m in moves:
+        c = 0
+        for i in range(n):
+            if board[i][m - 1] > 0:
+                c = 1
+                break
+        if c == 0:
+            continue
+        x = board[i][m - 1]
+        board[i][m - 1] = 0
+
+        if stack and stack[-1] == x:
+            stack.pop()
+            answer += 2
+        else:
+            stack.append(x)
+
+    return answer
+
+board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+moves = [1, 2, 3, 4]
+print(solution(board, moves))
