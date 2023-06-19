1b81c91abc74a202926d215fe93678c59b89ed0a
HanWool Jeon <wjsgksdnf11@naver.com>
2023-06-19 22:01:43 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,6 +1,5 @@
 def solution(array, commands):
     answer = []
     for i, j, k in commands:
-        arr = sorted(array[i - 1:j])
-        answer.append(arr[k - 1])
+        answer.append(sorted(array[i - 1:j])[k - 1])
     return answer
