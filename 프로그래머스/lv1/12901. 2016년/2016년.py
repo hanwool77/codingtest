d4afdaf3a5b31ea9777fa20d7964ee988f284e2c
HanWool Jeon <wjsgksdnf11@naver.com>
2023-07-24 20:37:15 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,5 +1,4 @@
 def solution(a, b):
-    answer = ''
     month = {
         1:31,
         2:29,
@@ -14,11 +13,18 @@ def solution(a, b):
         11:30,
         12:31
     }
-    요일 = {1:"FRI", 2:"SAT", 3:"SUN", 4:"MON", 5:"TUE", 6:"WED", 0:"THU"}
     
-    day = 0
-    for i in range(a - 1):
-        day += month[i + 1]
-    day += b
-    print(day % 7)
-    return 요일[day % 7]
+    week = {
+        1:"FRI",
+        2:"SAT",
+        3:"SUN",
+        4:"MON",
+        5:"TUE",
+        6:"WED",
+        0:"THU"
+    }
+    
+    day = b
+    for i in range(1, a):
+        day += month[i]
+    return week[day % 7]
