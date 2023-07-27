317fd97f249473569dd057683c90c96f11b33912
HanWool Jeon <wjsgksdnf11@naver.com>
2023-07-27 20:00:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,5 +1,9 @@
 def solution(n, arr1, arr2):
     answer = []
-    for i, j in zip(arr1, arr2):
-        answer.append((bin(i | j)[2:]).zfill(n).replace('1', '#').replace('0',' '))
+    
+    for i in range(n):
+        a = (bin(arr1[i] | arr2[i])[2:]).rjust(n, '0')
+        a = a.replace('1', '#').replace('0', ' ')
+        answer.append(a)
+        
     return answer
