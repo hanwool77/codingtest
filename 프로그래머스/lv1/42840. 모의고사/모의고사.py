67d1fb4a2b205be6d33720d3127c5bd28c5b6dff
HanWool Jeon <wjsgksdnf11@naver.com>
2023-08-07 20:56:47 +0900

    [level 1] Title: 모의고사, Time: 3.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -2,19 +2,19 @@ def solution(answers):
     one = [1, 2, 3, 4, 5]
     two = [2, 1, 2, 3, 2, 4, 2, 5]
     three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    score = [0] * 3
+    arr = [0, 0, 0]
     
-    for i, answer in enumerate(answers):
-        if one[i % 5] == answer:
-            score[0] += 1
-        if two[i % 8] == answer:
-            score[1] += 1
-        if three[i % 10] == answer:
-            score[2] += 1
+    for i, a in enumerate(answers):
+        if one[i % len(one)] == a:
+            arr[0] += 1
+        if two[i % len(two)] == a:
+            arr[1] += 1
+        if three[i % len(three)] == a:
+            arr[2] += 1
     
     answer = []
-    for i, s in enumerate(score):
-        if score[i] == max(score):
-            answer.append((i + 1))
-    return answer
+    for i, a in enumerate(arr, start = 1):
+        if a == max(arr):
+            answer.append(i)
     
+    return answer
