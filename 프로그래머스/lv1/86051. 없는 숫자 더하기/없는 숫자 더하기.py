f8c093b1a5f1a5cae21e2b20e6fbfde3c9c109b4
HanWool Jeon <wjsgksdnf11@naver.com>
2023-08-17 20:22:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -6,3 +6,12 @@ def solution(numbers):
             answer += i
             
     return answer
+
+
+
+
+# def solution(numbers):
+#     집합 = set([i for i in range(10)])
+#     numbers = set(numbers)
+    
+#     return sum(집합.difference(numbers))
